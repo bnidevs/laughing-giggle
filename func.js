bf26a17aff407838f26886b4c56ad2144aa55b0f
Bill Ni <bni@stuy.edu>
2019-01-10 17:04:13 -0500

    Create func.js

---
+++ func.js
@@ -0,0 +1,7 @@
+var func = () => {
+  var elements = document.getElementsByClassName("yj6qo ajU");
+  for(var i = 0; i < elements.length; i++){
+    elements[i].click();
+    elements[i].innerHTML = "";
+  }
+}
