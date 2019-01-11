16b685f636dd27b8301138b642829f48cbd7a398
Bill Ni <bni@stuy.edu>
2019-01-11 08:26:38 -0500

    improved in case not all expanded during first iteration

--- func.js
+++ func.js
@@ -1,7 +1,10 @@
 var func = () => {
-  var elements = document.getElementsByClassName("yj6qo ajU");
-  for(var i = 0; i < elements.length; i++){
-    elements[i].click();
-    elements[i].remove();
-  }
+	var elements = document.getElementsByClassName("yj6qo ajU");
+	while(elements.length > 0){
+  		for(var i = 0; i < elements.length; i++){
+    		elements[i].click();
+    		elements[i].remove();
+  		}
+		elements = document.getElementsByClassName("yj6qo ajU");
+    }
 }
