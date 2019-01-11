d823217120b14b81ee5c195a9930eb71c0e354f0
Bill Ni <bni@stuy.edu>
2019-01-11 08:30:32 -0500

    correct indentation

--- func.js
+++ func.js
@@ -2,9 +2,9 @@ var func = () => {
 	var elements = document.getElementsByClassName("yj6qo ajU");
 	while(elements.length > 0){
   		for(var i = 0; i < elements.length; i++){
-    		elements[i].click();
-    		elements[i].remove();
+    			elements[i].click();
+    			elements[i].remove();
   		}
 		elements = document.getElementsByClassName("yj6qo ajU");
-    }
+    	}
 }
